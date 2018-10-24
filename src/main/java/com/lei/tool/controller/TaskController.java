54199d13d7e2c328f415438d25b90c907c8bee0e
sun3stones <32724669+sun3stones@users.noreply.github.com>
2018-10-25 00:52:01 +0800

    task-4

--- src/main/java/com/lei/tool/controller/TaskController.java
+++ src/main/java/com/lei/tool/controller/TaskController.java
@@ -2,12 +2,17 @@ package com.lei.tool.controller;
 
 import com.lei.tool.dto.TaskDto;
 import com.lei.tool.dto.UserDto;
+import com.lei.tool.entity.ProjectGroup;
+import com.lei.tool.entity.UUser;
 import com.lei.tool.service.ProjectTaskService;
 import com.lei.tool.utils.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 @RequestMapping("/task")
@@ -17,15 +22,18 @@ public class TaskController extends BaseController {
     private ProjectTaskService taskService;
 
     @RequestMapping("/taskList")
-    public String taskList(){
+    public String taskList(HttpServletRequest request){
+        UUser user = getUser();
+        List<ProjectGroup> projectList = taskService.getInitProject(user);
+        request.setAttribute("projectList",projectList);
         return "task/taskList";
     }
 
     @RequestMapping("/taskDataList")
     @ResponseBody
     public Page<TaskDto> taskDataList(Page<TaskDto> page,TaskDto taskDto){
-        UserDto userDto = getUserDto();
-        taskDto.setUid(userDto.getId());
+        UUser user = getUser();
+        taskDto.setUid(user.getId());
         return taskService.getTaskPage(page,taskDto);
     }
 
